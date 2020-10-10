8a90a96619e76535d108fdfdb193eff91cfaad14
Gil Orcilla <mathnasiumcos@gmail.com>
2020-10-09 19:14:47 -0600

    name all variables and write the arrays

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,8 +7,30 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+function generatePassword() {
+  // array
+  var selected = [];
+
+  // asking user password preference
+  var upperCase = confirm("Select upper case letters?");
+  var lowerCase = confirm("Select lower case letters?");
+  var numerals = confirm("Select numerals?");
+  var specialCharacter = confirm("Select special characters?");
+  console.log(upperCase);
+}
+
+
+
+
+// array for pass criteria
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacter = [" ", "!", " " ", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "\", "^", "_", "`", "{", "|", "}", "~" ];
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
